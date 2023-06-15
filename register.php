25130dc1228aadba47a555599b5bbd0d6fbc1c67
kamalesh <vrkamalesh13@gmail.com>
2023-06-16 02:14:29 +0800

    first commit

---
+++ register.php
@@ -0,0 +1,87 @@
+<?php
+
+@include ' connection.php';
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $user_type = $_POST['user_type'];
+   $user_type = $_POST['select_type'];
+
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $error[] = 'user already exist!';
+
+   }else{
+
+      if($pass != $cpass){
+         $error[] = 'password not matched!';
+      }else{
+         $insert = "INSERT INTO user_form(name, email, password, user_type, select_type) VALUES('$name','$email','$pass','$user_type','select_type')";
+         mysqli_query($conn, $insert);
+         header('location:user_page.php');
+      }
+   }
+
+};
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>VEHICLE REGISTRATION</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="text" name="name" required placeholder="enter your name">
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="password" name="cpassword" required placeholder="confirm your password">
+      <select name="user_type">
+         <option value="user">user</option>
+         <option value="admin">admin</option>
+      </select>
+      <select name="select_type">
+         <option value="Have vehicle License">Have Vehicle License</option>
+         <option value="Dosen`t vehicle have license">Dosen`t Have Vehicle License</option>
+         </select>
+         <select name="select_type">
+         <option value="Have vehicle License">Have Vehicle Grant</option>
+         <option value="vehicle grant">Dosen`t Have Vehicle Grant</option>
+      </select>
+
+      
+      <p>already have an account? <a href="sucessfull.php">SUBMIT</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
---
+++ sucessfull.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>sucessfull</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+   <div class="content">
+      <p>YOUR REGISTRATION HAS BEEN CHECKED</p>
+      <a href="index.php" class="btn">SUCESSFULLY</a>
+   </div>
+
+</div>
+
+</body>
+</html>
